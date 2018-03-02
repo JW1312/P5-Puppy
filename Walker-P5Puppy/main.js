19e601f4e874c8923fbb8f09ce1bed6661433ba2
JW1312 <33159712+JW1312@users.noreply.github.com>
2018-03-02 11:22:28 -0600

    Add files via upload

---
+++ Walker-P5Puppy/main.js
@@ -0,0 +1,43 @@
+function setup(){
+    createCanvas(1000,1000);
+}
+
+function draw(){
+    background(50, 55, 100);
+ fill(139,69,19);
+    ellipse(500,250, 250,250);
+fill(210,180,140);
+    ellipse(500,275, 200,200);
+fill(0);
+    ellipse(450,250, 50,50);
+    ellipse(550,250, 50,50);
+  fill(255);
+    ellipse(432,250, 10,10);
+    ellipse(532,250, 10,10);
+fill(139,69,19);
+    ellipse(349,220, 75,75);
+    ellipse(651,220, 75,75);
+fill(210,180,140);
+    ellipse(360,220, 50,50);
+    ellipse(640,220, 50,50);
+fill(0);
+    ellipse(500,280, 30,10);
+fill(255);
+    ellipse(500,330, 40,40);
+fill(139,69,19);
+    rect(480,372, 40,10);
+fill(139,69,19);
+    rect(450,382, 100,180);
+fill(210,180,140);
+    ellipse(500,460,75,120);
+fill(139,69,19);
+    rect(450,562, 40,60);
+fill(139,69,19);
+    rect(510,562,40,60);
+fill(139,69,19);
+    rect(410,350, 40,60);
+fill(139,69,19);
+    rect(550,350,40,60);
+fill(255, 204, 0);
+    arc(mouseX, mouseY, 50, 50, 0, PI+QUARTER_PI, PIE);
+}
